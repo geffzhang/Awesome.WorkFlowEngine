4462e4a18cf15be4141b65be074c10e01c328cb2
多态 <xufenghui@126.com>
2018-02-23 23:02:04 +0800

    git rm -r --cached .

--- Core/WorkFlowActorUser.cs
+++
@@ -1,9 +0,0 @@
-namespace Walle.WorkFlowEngine.Core
-{
-    public class WorkFlowActorUser
-    {
-        public string Category {get;set;} = string.Empty;
-        public string Label {get;set;} = string.Empty;
-        public string Value {get;set;} = string.Empty;
-    }
-}
--- Core/WorkFlowNodeSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Walle.WorkFlowEngine.Core
-{
-    public class WorkFlowNodeSettings
-    {
-        public string Label { get; set; } = string.Empty;
-        public string Value { get; set; } = string.Empty;
-        public string Key { get; set; } = string.Empty;
-    }
-}
